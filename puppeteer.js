5c1bfef54391e0d24de06d75d6cfed4f52eee1e9
Meathill <meathill@gmail.com>
2019-12-23 22:36:55 +0800

    change: try to get rules of a node

---
+++ puppeteer.js
@@ -0,0 +1,70 @@
+const {promises: {writeFile}} = require('fs');
+const puppeteer = require('puppeteer');
+
+function sleep(duration) {
+  return new Promise(resolve => {
+    setTimeout(resolve, duration * 1000);
+  });
+}
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  const session = await page.target().createCDPSession();
+
+  await session.send('DOM.enable');
+  await session.send('CSS.enable');
+  await session.send('Page.enable');
+
+  await session.on('Page.domContentEventFired', (...args) => {
+    console.log('dom event: ', args);
+  });
+  await session.on('Page.lifecycleEvent', (...args) => {
+    console.log('lifecycle event: ', args);
+  });
+  await session.on('Page.loadEventFired', async (...args) => {
+    console.log('load event: ', args);
+
+    const {root} = await session.send('DOM.getDocument', {
+      depth: -1,
+    });
+
+    const {nodeId} = root;
+    const {nodeIds} = await session.send('DOM.querySelectorAll', {
+      nodeId,
+      selector: 'h1',
+    });
+    console.log('H1: ', nodeIds);
+
+    let promises = nodeIds.map(async nodeId => {
+      const html = await session.send('DOM.getOuterHTML', {
+        nodeId,
+      });
+      console.log(`Node ID ${nodeId}: `, html);
+
+      const result = await session.send('CSS.getMatchedStylesForNode', {
+        nodeId,
+      });
+      const {matchedCSSRules} = result;
+      await writeFile(`tmp/${nodeId}.json`, JSON.stringify(matchedCSSRules, null, '  '), 'utf8');
+    });
+    await Promise.all(promises);
+  });
+
+  await session.send('Page.navigate', {
+    url: 'https://openresty.com.cn/cn/',
+  });
+
+  //const screenshot = await session.send('Page.captureScreenshot');
+  //await writeFile('tmp/1.png', screenshot, 'base64');
+
+  await sleep(10);
+
+  const screenshot = await session.send('Page.captureScreenshot');
+  await writeFile(`tmp/1.png`, screenshot, 'utf8');
+
+  console.log('done.');
+  browser.close();
+})();
+
--- sessions.js
+++ sessions.js
@@ -18,23 +18,73 @@ const SEND = require('./SEND');
   const pageTarget = targetsResponse.result.targetInfos.find(info => info.type === 'page');
 
   // Attach to the page target.
-  const sessionId = (await SEND(ws, {
+  let response = await SEND(ws, {
     id: 2,
     method: 'Target.attachToTarget',
     params: {
       targetId: pageTarget.targetId,
       flatten: true,
     },
-  })).result.sessionId;
+  });
+  const sessionId = response.result.sessionId;
 
   // Navigate the page using the session.
   await SEND(ws, {
     sessionId,
-    id: 1, // Note that IDs are independent between sessions.
+    id: 3, // Note that IDs are independent between sessions.
     method: 'Page.navigate',
     params: {
-      url: 'https://pptr.dev',
+      url: 'https://openresty.com.cn/cn/',
     },
   });
+
+  const {result: {root}} = await SEND(ws, {
+    sessionId,
+    id: 4,
+    method: 'DOM.getDocument',
+    params: {
+      depth: 10,
+    },
+  });
+
+  const {nodeId} = root;
+  const {result: {nodeIds}} = await SEND(ws, {
+    sessionId,
+    id: 5,
+    method: 'DOM.querySelectorAll',
+    params: {
+      nodeId,
+      selector: 'h1',
+    },
+  });
+  console.log('H1: ', nodeIds);
+
+  let promises = nodeIds.map(async (nodeId, index) => {
+    const html = await SEND(ws, {
+      sessionId,
+      id: 6 + index,
+      method: 'DOM.getOuterHTML',
+      params: {
+        nodeId,
+      },
+    });
+    console.log(`Node ID ${nodeId}: `, html);
+  });
+
+  await Promise.all(promises);
+
+  promises = nodeIds.map(async (nodeId, index) => {
+    const result = await SEND(ws, {
+      sessionId,
+      id: 8 + index,
+      method: 'CSS.getMatchedStylesForNode',
+      params: {
+        nodeId,
+      },
+    });
+    console.log(`Node ID ${nodeId}: `, result);
+  });
+
+  await Promise.all(promises);
 })();
 
